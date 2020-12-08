02edfff19c60f04641ec690dc542418311cb1c32
Minakshi Mukherjee <adaboost.one@gmail.com>
2020-12-08 01:38:12 -0800

    Create insertionSort.cpp

---
+++ sort/insertionSort.cpp
@@ -0,0 +1,34 @@
+/* This program implements Insertion sort algorithm for a vectyor of integers.
+* It starts with index 1 and if the previous element is greater then the current
+* element, it shifts the previous to the right and inserts the currenmt to the
+* left.
+* Worst case performance of this algorithm is O(n^2)
+* Best case performance of this algorithm is O(n).
+* This program uses Stanford C++  library.
+*/
+#include <iostream>
+#include "testing/SimpleTest.h"
+#include "testing/TextUtils.h"
+#include "vector.h"
+
+using namespace std;
+
+void insertionSort(Vector<int> &vec){
+    for (int i =1; i < vec.size(); i++) {
+        int temp = vec[i];
+        int j = i;
+        while (j >=1 && vec[j-1] > temp){
+            vec[j] = vec[j-1];
+            j--;
+        }
+        vec[j] = temp;
+
+    }
+}
+
+
+STUDENT_TEST("Test sort when minimum at the end"){
+    Vector<int> v = {5, 4, 6, 7, 8, 3, 0, 11, 34, 2, 1};
+    insertionSort(v);
+    cout << v << endl;
+}

c8254522a3b77017eecf15f30bc5b5641fec79ba
Minakshi Mukherjee <adaboost.one@gmail.com>
2021-01-22 17:33:20 -0800

    Create xyz_there.py

---
+++ xyz_there.py
@@ -0,0 +1,44 @@
+# Return True if the given string contains an appearance of "xyz" where the xyz is not directly
+# preceeded by a period (.). So "xxyz" counts but "x.xyz" does not.
+#
+#
+# xyz_there('abcxyz') → True
+# xyz_there('abc.xyz') → False
+# xyz_there('xyz.abc') → True
+
+def xyz_there(str):
+  for i in range(len(str)):
+    if str[i] != '.' and str[i+1:i+4] == 'xyz':
+      return True
+  if str[0:3] == 'xyz':
+    return True
+  return False
+
+if xyz_there('abcxyz') == True:
+    print("OK")
+if xyz_there('abc.xyz') == False:
+    print("OK")
+if xyz_there('xyz.abc') == True:
+    print("OK")
+if xyz_there('abcxy') == False:
+    print("OK")
+if xyz_there('xyz') == True:
+    print("OK")
+if xyz_there('xy') == False:
+    print("OK")
+if xyz_there('x') == False:
+    print("OK")
+if xyz_there('') == False:
+    print("OK")
+if xyz_there('abc.xyzxyz') == True:
+    print("OK")
+if xyz_there('abc.xxyz') == True:
+    print("OK")
+if xyz_there('.xyz') == False:
+    print("OK")
+if xyz_there('12.xyz') == False:
+    print("OK")
+if xyz_there('12xyz') == True:
+    print("OK")
+if xyz_there('1.xyz.xyz2.xyz') == False:
+    print("OK")
